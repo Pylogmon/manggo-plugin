7922794ccc2c1be765be1d0a14d326aa119fb362
Pylogmon <pylogmon@outlook.com>
2026-07-05 22:36:32 +0800

    fix package script

--- package.sh
+++ package.sh
@@ -8,6 +8,6 @@ OUT_FILE="$OUT_DIR/com.manggo.examples.openai-compatible-services.mplugin"
 mkdir -p "$OUT_DIR"
 rm -f "$OUT_FILE"
 cd "$ROOT"
-zip -r "$OUT_FILE" manggo.plugin.json main.js icon.svg README.md
+zip -r "$OUT_FILE" manggo.plugin.json main.js icon.png README.md
 
 echo "$OUT_FILE"
